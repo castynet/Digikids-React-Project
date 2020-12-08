006776220ce94d22e6a1695777bd442b20b15b19
Riunge Maina <51113345+castynet@users.noreply.github.com>
2020-12-08 10:38:24 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+import Hello from "./components/hello";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Hello />
+    </div>
+  );
+}
