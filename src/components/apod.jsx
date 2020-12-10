ee392b563bafcab9eb78b9e1be4b279999ae0f65
Riunge Maina <51113345+castynet@users.noreply.github.com>
2020-12-10 10:32:44 +0300

    async get data from Nasa

--- src/components/apod.jsx
+++ src/components/apod.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Apod() {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const rawData = await fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=1DUyIPZRNvevTAO9w0v3tShxDK2zjbdny8B4Asp4"
+    );
+    const data = await rawData.json();
+  };
+
   return (
     <>
       <p>hello</p>
