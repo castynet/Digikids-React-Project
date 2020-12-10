fb299a67434c86611b86abb95ba5c43111bb87ea
Riunge Maina <51113345+castynet@users.noreply.github.com>
2020-12-10 09:34:58 +0300

    start of nasa app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./styles.css";
 
-import Hello from "./components/hello";
 
 export default function App() {
   return (
     <div className="App">
-      <Hello />
+      
     </div>
   );
 }
