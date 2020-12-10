41232edcaa23a63f168c31b871adbbcf98a1b977
Riunge Maina <51113345+castynet@users.noreply.github.com>
2020-12-10 10:09:32 +0300

    added general structure & imported Apod

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./styles.css";
 
+import Apod from "./components/apod";
 
 export default function App() {
   return (
     <div className="App">
-      
+      <Apod />
     </div>
   );
 }
---
+++ src/components/apod.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Apod() {
+  return (
+    <>
+      <p>hello</p>
+    </>
+  );
+}
